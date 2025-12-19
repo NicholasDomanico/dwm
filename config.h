8adcc8242c1bfc8c53a188ef6bb6088713278ea2
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2025-12-19 00:45:23 -0500

    Add Noto Color Emoji font as secondary font to provide better support for emojis

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "Noto Color Emoji:size=11"};
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
